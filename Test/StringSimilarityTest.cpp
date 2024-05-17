144b6ee4f8f120abf4a174febf25e8f12ce08c2b
Youngjoo <my.cat.liang@gmail.com>
2024-05-17 15:49:57 +0900

    [feature] add zero score tests

--- Test/StringSimilarityTest.cpp
+++ Test/StringSimilarityTest.cpp
@@ -8,3 +8,37 @@ TEST(StringSimilarityTest, StringsWithSameLength) {
 	
 	EXPECT_EQ(60, stringSmilarity.getScore(left, right));
 }
+
+TEST(StringSimilarityTest, ZeroLengthRight) {
+	StringSimilarity stringSmilarity{};
+	string right = "";
+	string left = "kjihgfedcba";
+
+	EXPECT_EQ(0, stringSmilarity.getScore(left, right));
+}
+
+
+TEST(StringSimilarityTest, ZeroLengthLeft) {
+	StringSimilarity stringSmilarity{};
+	string right = "abcdefghijk";
+	string left = "";
+
+	EXPECT_EQ(0, stringSmilarity.getScore(left, right));
+}
+
+TEST(StringSimilarityTest, TwiceLengthRight) {
+	StringSimilarity stringSmilarity{};
+	string right = "abcdefghijkabcdefghijk";
+	string left = "kjihgfedcba";
+
+	EXPECT_EQ(0, stringSmilarity.getScore(left, right));
+}
+
+
+TEST(StringSimilarityTest, TwiceLengthLeft) {
+	StringSimilarity stringSmilarity{};
+	string right = "abcdefghijk";
+	string left = "kjihgfedcbakjihgfedcba";
+
+	EXPECT_EQ(0, stringSmilarity.getScore(left, right));
+}
